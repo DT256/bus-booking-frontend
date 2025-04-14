1540af51cf5dcd06251962d03f74cacd0587113e
dt256 <buiducthang13022004@gmail.com>
2025-04-14 22:52:54 +0700

    search layout

---
+++ app/src/main/java/com/group8/busbookingapp/activity/SearchResultActivity.java
@@ -0,0 +1,19 @@
+package com.group8.busbookingapp.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.group8.busbookingapp.R;
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search_result);
+    }
+}
